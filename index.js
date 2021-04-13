ad3a298839387514e9af4b66300655c8be7b9b5f
Zhantore <753gg@mail.ru>
2021-04-13 15:58:29 +0600

    server created/User model/DB schema

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log("Server started");
+const express = require('express');
+const mongoose = require('mongoose');
+const config = require('config');
+
+const app = express();
+const PORT = config.get('serverPort'); // Порт берется из папки config
+const start = async () =>{
+    try{
+        await mongoose.connect(config.get('dbUrl'));
+
+        app.listen(PORT, ()=>{
+            console.log('Server started on port ', PORT)
+        });
+    } catch(e){
+        
+    }
+}
+
+start();
